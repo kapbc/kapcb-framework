a5df08dae6a2adf81e3f953261224203bc3c326d
kapcb <eircccallroot@yeah.net>
2021-07-05 21:52:22 +0800

    framework

---
+++ src/main/java/kapcb/framework/web/annotation/inject/EnableRabbitService.java
@@ -0,0 +1,24 @@
+package kapcb.framework.web.annotation.inject;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <a>Title: EnableRabbitService </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description:  <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/7/5 21:49
+ */
+@Inherited
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EnableRabbitService {
+}
