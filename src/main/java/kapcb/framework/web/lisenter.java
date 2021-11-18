0b4e1c76858296b42d81a844152b2026f9f5985d
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-11-18 22:38:02 +0800

    application listener

---
+++ src/main/java/kapcb/framework/web/lisenter.java
@@ -0,0 +1,13 @@
+package kapcb.framework.web;
+
+/**
+ * <a>Title: lisenter </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: lisenter <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/11/18 22:34
+ */
+public class lisenter {
+}
---
+++ src/main/java/kapcb/framework/web/listener/CustomApplicationListener.java
@@ -0,0 +1,26 @@
+package kapcb.framework.web.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.stereotype.Component;
+
+/**
+ * <a>Title: CustomApplicationListener </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: CustomApplicationListener <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/11/18 22:35
+ */
+@Slf4j
+@Component
+public class CustomApplicationListener implements ApplicationListener<ApplicationStartedEvent> {
+
+    @Override
+    public void onApplicationEvent(ApplicationStartedEvent applicationStartedEvent) {
+        ConfigurableEnvironment environment = applicationStartedEvent.getApplicationContext().getEnvironment();
+    }
+}
