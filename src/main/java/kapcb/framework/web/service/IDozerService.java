d3e295b7d5d441c8a5180172aa010ce938ad70d8
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-11-20 22:38:19 +0800

    framework

--- src/main/java/kapcb/framework/web/service/IDozerService.java
+++ src/main/java/kapcb/framework/web/service/IDozerService.java
@@ -1,6 +1,9 @@
 package kapcb.framework.web.service;
 
-import org.apache.poi.ss.formula.functions.T;
+import com.kapcb.framework.common.page.IPageResult;
+import com.kapcb.framework.common.page.PageResult;
+
+import java.util.List;
 
 /**
  * <a>Title: IDozerService </a>
@@ -24,5 +27,37 @@ public interface IDozerService {
      */
     <V, P> V toVO(P p, Class<V> clazz);
 
+    /**
+     * convert po list to vo list
+     *
+     * @param p     po
+     * @param clazz Class<V>
+     * @param <V>   <V>
+     * @param <P>   <p>
+     * @return List<V>
+     */
+    <V, P> List<V> toVOList(List<P> p, Class<V> clazz);
+
+    /**
+     * convert page result
+     *
+     * @param pageResult PageResult<P>
+     * @param clazz      Class<V>
+     * @param <V>        <V>
+     * @param <P>        <p>
+     * @return PageResult<V>
+     */
+    <V, P> IPageResult<V> toPage(PageResult<P> pageResult, Class<V> clazz);
+
+    /**
+     * convert to array
+     *
+     * @param p     p
+     * @param clazz Class<V>
+     * @param <V>   <V>
+     * @param <P>   <p>
+     * @return V[]
+     */
+    <V, P> V[] toArray(P[] p, Class<V> clazz);
 
 }
--- src/main/java/kapcb/framework/web/service/impl/DozerServiceImpl.java
+++ src/main/java/kapcb/framework/web/service/impl/DozerServiceImpl.java
@@ -1,6 +1,20 @@
 package kapcb.framework.web.service.impl;
 
+import com.kapcb.framework.common.page.IPageResult;
+import com.kapcb.framework.common.page.PageResult;
+import com.kapcb.framework.common.util.PageResultUtil;
 import kapcb.framework.web.service.IDozerService;
+import org.apache.commons.collections.CollectionUtils;
+import org.dozer.Mapper;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * <a>Title: DozerServiceImpl </a>
@@ -11,10 +25,42 @@ import kapcb.framework.web.service.IDozerService;
  * @version 1.0.0
  * @date 2021/11/20 16:52
  */
+@Lazy
+@Component
 public class DozerServiceImpl implements IDozerService {
 
+    @Resource
+    private Mapper mapper;
+
     @Override
-    public <V, P> V toVO(P p, Class<V> clazz) {
-        return null;
+    public <V, P> V toVO(P p, @NonNull Class<V> clazz) {
+        return Objects.nonNull(p) ? this.mapper.map(p, clazz) : null;
+    }
+
+    @Override
+    public <V, P> List<V> toVOList(List<P> p, @NonNull Class<V> clazz) {
+        return CollectionUtils.isNotEmpty(p) ? p.stream().map(e -> this.mapper.map(e, clazz)).collect(Collectors.toList()) : null;
+    }
+
+    @Override
+    public <V, P> IPageResult<V> toPage(@NonNull PageResult<P> pageResult, @NonNull Class<V> clazz) {
+        List<P> records = pageResult.getRecords();
+        List<V> result = null;
+        if (CollectionUtils.isNotEmpty(records)) {
+            result = toVOList(records, clazz);
+        }
+        return PageResultUtil.restPage(result, pageResult);
+    }
+
+    @Override
+    public <V, P> V[] toArray(P[] p, Class<V> clazz) {
+        if (Objects.isNull(p)) {
+            return null;
+        }
+        V[] resultArray = (V[]) Array.newInstance(clazz, p.length);
+        for (int i = 0; i < p.length; i++) {
+            resultArray[i] = this.mapper.map(p[i], clazz);
+        }
+        return resultArray;
     }
 }
